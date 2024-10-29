7aa63cfe88fc95eded4e616bd14d73d29ed234c4
“sherySJ” <sherjan909@gmail.com>
2024-10-29 19:59:56 +0500

    Refactor tasks controller and module
    - Added a GET endpoint to retrieve all tasks in the TasksController
    - Injected TasksService into the TasksController constructor
    - Added TasksService as a provider in the TasksModule
    - Created a Task interface in task.modal.ts
    - Created a TaskStatus enum in task.modal.ts
    - Implemented getAllTasks method in TasksService

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from './task.modal';
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+}

0e7c763eb144bf7b59f83e180c2a2e0bbc3131b4
“sherySJ” <sherjan909@gmail.com>
2024-10-29 21:50:15 +0500

    Add uuid dependency and createTask endpoint in tasks controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.modal';
+import { Task, TaskStatus } from './task.modal';
+import { v7 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(CreateTaskDto): Task {
+    const { title, description } = CreateTaskDto; //destructure so we can use title and description directly
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }

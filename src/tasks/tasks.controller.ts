dade6f560fded66406c971fd0b223a8c041afb1a
“sherySJ” <sherjan909@gmail.com>
2024-10-30 19:38:37 +0500

    Refactor tasks module: replace task.modal with task.model, implement task filtering, and add task retrieval and deletion endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,26 +1,56 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.modal';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskByID(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id); //my implementation
+  }
+
+  // @Delete('/:id')
+  // remove(@Param('id') id: string) {
+  //   return this.remove(id);    //my implementation
+  // }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaksStatus(id, status);
   }
 
   @Post()
-  createTask(
-    @Body() CreateTaskDto: CreateTaskDto,
-    // @Body('title') title: string, //deprecated, use dto instead
-    // @Body('description') description: string,
-  ): Task {
-    // console.log('title', title);
-    // console.log('description', description);
-
-    // return this.tasksService.createTask(title, description);
+  createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(CreateTaskDto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { Task, TaskStatus } from './task.modal';
+import { Task, TaskStatus } from './task.model';
 import { v7 as uuid } from 'uuid';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+
+        return false;
+      });
+    }
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
   }
 
   createTask(CreateTaskDto): Task {
@@ -19,6 +45,20 @@ export class TasksService {
     };
 
     this.tasks.push(task);
+    return task;
+  }
+
+  // deleteTaskbyID(id: string): Task {
+  //    this.tasks.find((task) => task.id === id); //my bothched implementaiton of deletion
+  // }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaksStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
     return task;
   }
 }
